7f048dafce9368e3ff829cd83a0c481f88f233f0
vladimir.nechesanov <vladimir.nechesanov@gmail.com>
2020-07-21 03:59:11 +0300

    feat: added edit mode

--- src/EditeModeCard/EditeModeCard.tsx
+++ src/EditeModeCard/EditeModeCard.tsx
@@ -1,21 +1,88 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+import CloseSign from "../Assets/CloseSign.svg";
+
+const hintAnimation = keyframes`
+  from {
+  opacity: 0;
+  }
+
+  to {
+  opacity: 1;
+  }
+`;
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
   justify-content: center;
-  padding: 0;
   margin-bottom: 20px;
   width: 100%;
-  height: 100px;
-  border-radius: 2px;
-  background-color: #fbfeff;
-  box-shadow: 0px 9px 3px -2px #b1e5ff;
+  height: 150px;
+  border-radius: 5px;
+  background-color: #cafff5;
+  box-shadow: 0px 6px 3px 3px #b1e5ff;
+  animation: ${hintAnimation} 0.3s ${(props) => props.theme.animationTiming};
 `;
 
-const Card = () => {
-  return <Container />;
+const Input = styled.input`
+  width: 300px;
+  border: none;
+  border-radius: 2px;
+  border-bottom: 2px solid #a4e2ff;
+  outline: none;
+  margin-top: 20px;
+  background-color: transparent;
+`;
+
+const AddButton = styled.div`
+  display: flex;
+  justify-content: center;
+  min-height: 28px;
+  width: 80px;
+  margin-top: 14px;
+  border-radius: 2px;
+  background-color: #fcfcfc;
+  align-items: center;
+  font-size: 12px;
+  cursor: pointer;
+`;
+
+const Close = styled.div`
+  background: url(${CloseSign});
+  display: flex;
+  align-self: flex-end;
+  width: 15px;
+  height: 15px;
+  background-size: 100% 100%;
+  margin-right: 15px;
+  cursor: pointer;
+`;
+
+const Title = styled.div`
+  font-size: 15px;
+  color: #828282;
+`;
+
+type Props = {
+  handleChange: (e: any) => void;
+  addButtonPressed: () => void;
+  closeButtonPressed: () => void;
 };
 
-export default Card;
+const EditModeCard = (props: Props) => {
+  const { handleChange, addButtonPressed, closeButtonPressed } = props;
+
+  return (
+    <Container>
+      <Close onClick={closeButtonPressed} />
+      <Title>input task</Title>
+      <Input type="text" onChange={(e) => handleChange(e)} />
+      <AddButton onClick={addButtonPressed}>Add Task</AddButton>
+    </Container>
+  );
+};
+
+export default EditModeCard;
--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import _uniqueId from "lodash/uniqueId";
 
 import Card from "../Card/Card";
+import EditModeCard from "../EditeModeCard/EditeModeCard";
 import PlusSign from "../Assets/PlusSign.svg";
+import InfoLogo from "../Assets/InfoLogo.svg";
 
 export type CardType = {
   id: string;
@@ -14,22 +16,82 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
   padding: 15px;
   margin: 0;
   width: 500px;
   min-height: 130px;
   border-radius: 5px;
   background-color: #ddfaff;
-  box-shadow: 0px 8px 3px -2px #018aa0;
+  box-shadow: 0px 6px 3px 3px #70cede;
 `;
 
-const Add = styled.div`
+const TabBar = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const PlusButton = styled.div`
   background: url(${PlusSign});
   width: 15px;
   height: 15px;
   background-size: 100% 100%;
   margin-bottom: 15px;
+  cursor: pointer;
+`;
+
+const InfoSign = styled.div`
+  background: url(${InfoLogo});
+  min-width: 15px;
+  min-height: 15px;
+  background-size: 100% 100%;
+  margin-bottom: 15px;
+  cursor: pointer;
+`;
+
+const hintAnimation = keyframes`
+  from {
+  opacity: 0;
+  }
+
+  to {
+  opacity: 1;
+  }
+`;
+
+const Hint = styled.div`
+  position: relative;
+  margin-left: 15px;
+  align-self: center;
+  text-align: center;
+`;
+
+const PopUpBox = styled.div`
+  position: absolute;
+  visibility: hidden;
+  white-space: nowrap;
+  width: auto;
+  overflow: hidden;
+  display: flex;
+  height: 16px;
+  font-size: 11px;
+  line-height: 12px;
+  color: #a9a9a9;
+  border-radius: 16px 5px 5px 16px;
+  line-height: 16px;
+  padding: 8px 10px;
+  top: -8px;
+  left: -10px;
+  background-color: #fff;
+  box-shadow: 5px 12px 32px rgba(147, 147, 149, 0.2);
+
+  ${Hint}:hover & {
+    visibility: visible;
+    animation: ${hintAnimation} 0.3s ${(props) => props.theme.animationTiming};
+  }
+`;
+
+const HintText = styled.div`
+  margin-left: 8px;
 `;
 
 export const CardsContext = React.createContext<{
@@ -37,21 +99,38 @@ export const CardsContext = React.createContext<{
 }>({ cardsArray: [] });
 
 const Layout = () => {
+  const [isEditModeOpen, setEditModeState] = useState(false);
   const [cards, setCard] = useState([] as CardType[]);
+  const [taskText, setTaskText] = useState("");
 
-  const onRemoveClick = (filteredCards: CardType[]) => {
-    setCard(filteredCards);
+  const handleChange = (e: any) => {
+    setTaskText(e.target.value);
   };
 
-  const addClicked = () => {
+  const addButtonPressed = () => {
+    setEditModeState(false);
     const id: string = _uniqueId();
 
     setCard(
       cards.concat({
         id,
-        card: <Card id={id} onRemoveClick={onRemoveClick} />,
+        card: (
+          <Card id={id} taskTitle={taskText} onRemoveClick={onRemoveClick} />
+        ),
       })
     );
+  };
+
+  const editModeCloseButtonPressed = () => {
+    setEditModeState(false);
+  };
+
+  const onRemoveClick = (filteredCards: CardType[]) => {
+    setCard(filteredCards);
+  };
+
+  const plusButtonClicked = () => {
+    setEditModeState(true);
   };
 
   const renderCards = () => {
@@ -60,10 +139,32 @@ const Layout = () => {
     });
   };
 
+  const renterTabBar = () => {
+    return (
+      <TabBar>
+        <PlusButton onClick={() => plusButtonClicked()} />
+        <Hint>
+          <InfoSign />
+          <PopUpBox>
+            <InfoSign />
+            <HintText>click on the plus button to add a new card</HintText>
+          </PopUpBox>
+        </Hint>
+      </TabBar>
+    );
+  };
+
   return (
     <CardsContext.Provider value={{ cardsArray: cards }}>
       <Container>
-        <Add onClick={() => addClicked()} />
+        {renterTabBar()}
+        {isEditModeOpen && (
+          <EditModeCard
+            handleChange={handleChange}
+            addButtonPressed={addButtonPressed}
+            closeButtonPressed={editModeCloseButtonPressed}
+          />
+        )}
         {renderCards()}
       </Container>
     </CardsContext.Provider>
